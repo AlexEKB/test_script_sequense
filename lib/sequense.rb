89ac35efaab62d045f8014959950720b73c20b62
alexekb <pu4kom@mail.ru>
2018-12-11 14:21:38 +0500

    Sequense class create

---
+++ lib/sequense.rb
@@ -0,0 +1,33 @@
+class Sequense
+
+  VALUE = '1'
+
+  def initialize
+    puts
+    @value = VALUE
+    puts VALUE
+  end
+
+  def create_sequense
+    next_value = @value.chars.inject([]) do |array, x|
+      if array.any? && array.last.has_key?(x)
+        array.last[x] += 1
+      else
+        array << {x => 1}
+      end
+      array
+    end
+
+    @value = ''
+
+    next_value.map do |hash|
+      hash.each { |k,v| @value << "#{v}#{k}" }
+    end
+
+  end
+
+  def get_sequense
+    @value
+  end
+
+end
--- test_script_sequense.rb
+++ test_script_sequense.rb
@@ -1,24 +1,11 @@
+require_relative 'lib/sequense'
+
 puts "Тестовое задание, версия 0.1 beta"
 
-value = '1'
-puts value
+sequense = Sequense.new
 
 5.times do
-
-  next_value = value.chars.inject([]) do |array, x|
-    if array.any? && array.last.has_key?(x)
-      array.last[x] += 1
-    else
-      array << {x => 1}
-    end
-    array
-  end
-  
-  value = ''
-
-  next_value.map do |hash| 
-    hash.each { |k,v| value << "#{v}#{k}" }
-  end
-
-  puts value
+  sequense.create_sequense
+  puts sequense.get_sequense
 end
+
